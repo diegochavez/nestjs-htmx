01a27c3879fb4f12550c9eba9df9d1e8b439cc67
Diego <diego@volumens.com>
2024-01-21 21:07:57 +0100

    Update polling logic and display polling progress

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -32,13 +32,13 @@ export class AppController {
   @Render('api/polling.hbs')
   clicked(@Res() res) {
     this.pollingCount++;
-    if (this.pollingCount > 5) {
+    if (this.pollingCount > 10) {
       // HTTP response code 286 and the element will cancel the polling - HTMX docs
       res.status(286);
       this.pollingCount = 0;
     }
     const date = new Date().toUTCString();
-    return {time: date, authenticated: this.authenticated};
+    return {time: date, authenticated: this.authenticated, pollingPercent: this.pollingCount * 10};
   }
 
   @Get('api/auth')
